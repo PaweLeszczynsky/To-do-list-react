348bfde2c5ecdec9de4d177d86a0ae230edca907
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-29 19:52:58 +0200

    show/hide buttons operator changes

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,23 +1,23 @@
 import "./style.css";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllTasksDone }) => {
-    {!!tasks &&(
-        
-            <div className="interactionButtonsContainer">
-                <button
-                    onClick={toggleHideDone}
-                    className="interactionButtonsContainer__Button"
-                >
-                    {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
-                </button>
-                <button
-                    onClick={setAllTasksDone}
-                    disabled={tasks.every(({ done }) => done)}
-                    className="interactionButtonsContainer__Button">
-                    Ukończ wszystkie
-                </button>
-            </div>)
-
-    }
+    if (tasks.length === 0) {
+        return null;
+    };
+    return (
+        <div className="interactionButtonsContainer">
+            <button
+                onClick={toggleHideDone}
+                className="interactionButtonsContainer__Button"
+            >
+                {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
+            </button>
+            <button
+                onClick={setAllTasksDone}
+                disabled={tasks.every(({ done }) => done)}
+                className="interactionButtonsContainer__Button">
+                Ukończ wszystkie
+            </button>
+        </div>)
 };
 export default Buttons;
