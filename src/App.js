cc17f87bd44e5cac99d74562edc0397b620560b2
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-09-29 19:44:03 +0200

    get local storage tasks changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import MainContainer from "./MainContainer";
 import { useEffect, useState } from "react";
 
 function App() {
-  const tasksStorage = JSON.parse(localStorage.getItem("tasks"));
+  const tasksStorage = JSON.parse(localStorage.getItem("tasks")) || [];
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState(tasksStorage);
   useEffect(() => {
