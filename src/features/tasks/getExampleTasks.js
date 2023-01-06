ab09d0e497b9576d13beb6f1769552a3e829fc99
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-01-06 20:58:16 +0100

    download example tasks changes

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("exampleTasks.json");
+    const response = await fetch("https://raw.githubusercontent.com/PaweLeszczynsky/to-do-list-react/main/public/exampleTasks.json");
 
     if (!response.ok) {
         new Error(response.statusText);
