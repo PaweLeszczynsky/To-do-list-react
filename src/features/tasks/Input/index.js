63b0155f21e98d70da79dd2be6e4d35e2f88dad4
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-02-11 18:11:25 +0100

    created input element to general access for components

---
+++ src/features/tasks/Input/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+    padding: 10px;
+    border: 2px solid rgb(221, 221, 221);
+    font-weight: 300;
+    flex-grow: 1;
+`;
