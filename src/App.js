c8d75e480f359498a80b5b6c5f0d4e403d1b3443
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-02-11 18:03:46 +0100

    added routes to links using react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/authorPage";
+import Navigation from "./common/Navigation";
 
-const App = () => <Tasks />;
+const App = () => (
+    <HashRouter>
+        <Navigation
+            title="Navigation"
+        />
+        <Switch>
+            <Route path="/zadania/:id">
+                <TaskPage />
+            </Route>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/autor">
+                <AuthorPage />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
+    </HashRouter>
+);
 
 export default App;
