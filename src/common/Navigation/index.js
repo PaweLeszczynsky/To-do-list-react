2a44e6d0e8937cafabc8b4c0679a56afe78ee0ac
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-03-25 14:13:09 +0100

    created function to change navigation border opacity

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,20 +1,27 @@
 
 import { StyledNavLink, StyledNavigation, StyledNavList, StyledNavListItem } from "./styled";
+import { useState } from "react";
 
-const Navigation = () => (
-    <StyledNavigation>
-        <StyledNavList>
-            <StyledNavListItem>
-                <StyledNavLink activeClassName="active" to="/zadania">
-                    Zadania
-                </StyledNavLink>
-            </StyledNavListItem>
-            <StyledNavListItem>
-                <StyledNavLink activeClassName="active" to="/autor">
-                    O autorze
-                </StyledNavLink>
-            </StyledNavListItem>
-        </StyledNavList>
-    </StyledNavigation>
-)
+const Navigation = () => {
+    const [opacityNav, setOpacityNav] = useState(0);
+    const changeThemeOpacity = () => {
+        setOpacityNav(1);
+    };
+    return (
+        <StyledNavigation>
+            <StyledNavList>
+                <StyledNavListItem opacityNav={opacityNav} onMouseOver={() => { changeThemeOpacity() }}>
+                    <StyledNavLink activeClassName="active" to="/zadania">
+                        Zadania
+                    </StyledNavLink>
+                </StyledNavListItem>
+                <StyledNavListItem opacityNav={opacityNav} onMouseOver={() => { changeThemeOpacity() }}>
+                    <StyledNavLink activeClassName="active" to="/autor">
+                        O autorze
+                    </StyledNavLink>
+                </StyledNavListItem>
+            </StyledNavList>
+        </StyledNavigation>
+    )
+};
 export default Navigation;
